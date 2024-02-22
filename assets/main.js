aa220951f42f89eca2ba9537e124555e171f3839
Nostea <kiwiuchi@googlemail.com>
2024-02-22 10:48:12 +0100

    added code structuring, changed the grid layout

--- assets/main.js
+++ assets/main.js
@@ -1,28 +1,92 @@
-fetch("https://picsum.photos/v2/list")
-  .then((response) => response.json())
-  .then((data) => {
-    console.log(data);
+// paginierte API pattern
 
-    data.forEach((cardIMG) => {
-      // create a div, where I put all the content of the cards inside in the later code starting linen8
-      let imgItem = document.createElement("div");
+let pageCounter = 1;
+const baseURL = "https://picsum.photos/id/";
 
-      let img = document.createElement("img");
-      img.setAttribute("src", `${cardIMG.download_url}`);
-      imgItem.appendChild(img);
+const loadGallery = (limit) => {
+  fetch(`https://picsum.photos/v2/list?page=${pageCounter}&limit=${limit}`)
+    .then((response) => response.json())
+    .then((data) => {
+      pageCounter++;
+      //console.log(data);
 
-      //create p tag, attach data from api, append inside div (cardItem)
-      let imgAuthor = document.createElement("p");
-      imgAuthor.textContent = cardIMG.author;
-      imgItem.appendChild(imgAuthor);
+      data.forEach((cardIMG) => {
+        // create a div, where I put all the content of the cards inside in the later code starting linen8
 
-      let cardButtonLink = document.createElement("a");
-      cardButtonLink.setAttribute("href", `${cardIMG.url}`);
-      cardButtonLink.textContent = "See more";
-      imgItem.appendChild(cardButtonLink);
+        const imgURL = `${baseURL}${cardIMG.id}/1920/1080`;
+        let imgItem = document.createElement("div");
 
-      //aooend the fully assembled div(imgItem) into the gallery in html
-      document.querySelector(".grid-container").appendChild(imgItem);
+        /*
+        //- alternative in I'm sane
+        //create p tag, attach data from api, append inside div (cardItem)
+        createImgItem(imgURL, imgItem);
+        createAuthorTextItem(cardIMG.author, imgItem); //- takes author text from api, and the div container imgItem created above as container (see row42 for example)
+        createButtonLink(cardIMG.url, imgItem);
+        */
+
+        // - alternative in I'm a senior grand wizard
+        imgItem.appendChild(createImgItem2(imgURL));
+        imgItem.appendChild(createAuthorTextItem2(cardIMG.author));
+        imgItem.appendChild(createButtonLink2(cardIMG.url));
+
+        //aooend the fully assembled div(imgItem) into the gallery in html
+        document.querySelector(".grid-container").appendChild(imgItem);
+      });
     });
-  });
-//.catch((error) => console.error("Error while fetching", error));
+};
+
+/*
+//- alternative in I'm sane
+const createImgItem = (url, containerElement) => {
+  let img = document.createElement("img");
+  img.setAttribute("src", url);
+  containerElement.appendChild(img);
+};
+
+const createAuthorTextItem = (author, containerElement) => {
+  let imgAuthor = document.createElement("p"); // create <p> tag, assign it to variable imgAuthor
+  imgAuthor.textContent = author; //  text provided by parameter author assigned to (<p>tag / variable imgAuthor)
+  containerElement.appendChild(imgAuthor); // append previously created + filled (<p> tag / imgAuthor) to the provided container, in this case a parameter where to put it
+};
+
+const createButtonLink = (link, containerElement) => {
+  let cardButtonLink = document.createElement("a");
+  cardButtonLink.setAttribute("href", link);
+  cardButtonLink.textContent = "See more";
+  containerElement.appendChild(cardButtonLink);
+};
+
+*/
+
+//! alternative in I'm a grand wizard dr med phil
+const createImgItem2 = (url) => {
+  let img = document.createElement("img");
+  img.setAttribute("src", url);
+  return img;
+};
+
+const createAuthorTextItem2 = (author) => {
+  let imgAuthor = document.createElement("p"); // create <p> tag, assign it to variable imgAuthor
+  imgAuthor.textContent = author; //  text provided by parameter author assigned to (<p>tag / variable imgAuthor)
+  return imgAuthor;
+};
+
+const createButtonLink2 = (link) => {
+  let cardButtonLink = document.createElement("a");
+  cardButtonLink.setAttribute("href", link);
+  cardButtonLink.textContent = "See more";
+  return cardButtonLink;
+};
+
+// copy ende here
+
+loadGallery(30);
+/*
+//- example of what happens in row 21-22
+createAuthorTextItem("foobar", document.body);
+createAuthorTextItem("foobar", document.body);
+createAuthorTextItem("foobar", document.body);
+createAuthorTextItem("foobar", document.body);
+createAuthorTextItem("foobar", document.body);
+createAuthorTextItem("foobar", document.body);
+*/
